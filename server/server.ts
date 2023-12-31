ba47b4994c3cae6c65b363af491c9c2203c22da0
Audun Leganger <audun.leganger@gmail.com>
2023-12-31 21:29:52 +0100

    Update booking GET request and add DELETE endpoint for bookings

--- server/server.ts
+++ server/server.ts
@@ -22,6 +22,14 @@ const filterConflictingBookings = (recievedBookings: Booking[], existingBookings
     });
 }
 
+const printBookingArray = (bookings: Booking[]) => {
+    // Print the bookings in a readable format
+    console.log("Bookings:")
+    bookings.forEach(booking => {
+        console.log(`Time: ${booking.timeSlot}, Machine: ${booking.machineNumber}, Date: ${booking.dateString}, Person: ${booking.personName}`)
+    });
+}
+
 let existingBookings: Booking[] = [];
 
 const app = express();
@@ -33,7 +41,7 @@ app.use(express.json());
 app.get("/bookings", (req: Request, res: Response) => {
     const selectedDateString: string = req.query.date as string;
     const bookingsOnSelectedDate: Booking[] = existingBookings.filter(booking => booking.dateString === selectedDateString);
-    res.send(bookingsOnSelectedDate);
+    res.status(200).send(bookingsOnSelectedDate);
 });
 
 // Request to add a set of bookings to the server
@@ -56,16 +64,41 @@ app.post("/bookings", (req: Request, res: Response) => {
     existingBookings = [...existingBookings, ...nonConflictingBookings];
 });
 
+// Request to delete a set of bookings from the server
 app.delete("/bookings", (req: Request, res: Response) => {
+    console.log("DELETE")
     const clientPersonName = req.body.clientPersonName;
     const  recievedBookings = req.body.bookings;
     // Get the stored bookings that are at the same time as the recieved bookings
     const bookingsAtSameTime = filterConflictingBookings(recievedBookings, existingBookings);
     // Filter out the bookings that are made by the client
     const bookingsToRemove = bookingsAtSameTime.filter(booking => booking.personName === clientPersonName);
+
+    console.log("Recieved Bookings:")
+    printBookingArray(recievedBookings);
+    console.log("\nBookings at same time:")
+    printBookingArray(bookingsAtSameTime);
+    console.log("\nBookings to remove:")
+    printBookingArray(bookingsToRemove);
+    if (bookingsToRemove.length === 0) {
+        // If no bookings match existing bookings under client name, send an error
+        res.status(404).send({ bookings: [] });
+        return;
+    }
+    
+    else if (bookingsToRemove.length === recievedBookings.length) {
+        // If all bookings match existing bookings under client name, send the bookings
+        res.status(200).send({ bookings: bookingsToRemove });
+    }
+
+    else {
+        // If some bookings match existing bookings under client name, send the bookings
+        res.status(201).send({ bookings: bookingsToRemove });
+    }
     // Remove the filtered bookings from the stored bookings
-    existingBookings = existingBookings.filter(booking => !bookingsToRemove.includes(booking));
+    existingBookings = existingBookings.filter(booking => !bookingsToRemove.some(bookingToRemove => bookingToRemove.timeSlot === booking.timeSlot && bookingToRemove.machineNumber === booking.machineNumber && bookingToRemove.dateString === booking.dateString));
 })
+
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 })
